f517a5dfd0bdd2fc59b6e6106e3715de4830232b
OsamaRabie <o.rabie@tap.company>
2024-05-18 17:58:54 +0300

    Using latest logic as in PayButton

--- BenefitPayExampleApp/BenefitPayExampleApp/BenefitPayButtonExample.swift
+++ BenefitPayExampleApp/BenefitPayExampleApp/BenefitPayButtonExample.swift
@@ -7,6 +7,7 @@
 import UIKit
 import BenefitPay_iOS
 import Toast
+import CryptoKit
 
 class BenefitPayButtonExample: UIViewController {
     @IBOutlet weak var benefitPayButton: BenefitPayButton!
@@ -14,12 +15,18 @@ class BenefitPayButtonExample: UIViewController {
     
     
     
-    var dictConfig:[String:Any] = ["operator":["publicKey":"pk_test_HJN863LmO15EtDgo9cqK7sjS","hashString":"OSAMA"],
+    var dictConfig:[String:Any] = ["operator":["publicKey":"pk_test_HJN863LmO15EtDgo9cqK7sjS","hashString":BenefitPayButtonExample.generateTapHashString(publicKey: "pk_test_HJN863LmO15EtDgo9cqK7sjS", secretKey: "sk_test_Iep2c1RCtrTE8BMPVwzi4UxF", amount: 0.1, currency: "BHD", postUrl: "", transactionReference: "trx")],
+                                   "scope":"charge",
+                                   "transaction":["reference":"trx",
+                                                  "authorize":[
+                                                    "type":"VOID",
+                                                    "time":12
+                                                  ]],
                                    "order":["id":"",
                                             "amount":0.1,
                                             "currency":"BHD",
                                             "description": "Authentication description",
-                                            "reference":"",
+                                            "reference":"ordRef",
                                             "metadata":[:]],
                                    "invoice":["id":""],
                                    "merchant":["id":""],
@@ -69,6 +76,34 @@ class BenefitPayButtonExample: UIViewController {
         self.navigationController?.pushViewController(configCtrl, animated: true)
         
     }
+    
+    
+    /**
+         This is a helper method showing how can you generate a hash string when performing live charges
+         - Parameter publicKey:             The Tap public key for you as a merchant pk_.....
+         - Parameter secretKey:             The Tap secret key for you as a merchant sk_.....
+         - Parameter amount:                The amount you are passing to the SDK, ot the amount you used in the order if you created the order before.
+         - Parameter currency:              The currency code you are passing to the SDK, ot the currency code you used in the order if you created the order before. PS: It is the capital case of the 3 iso country code ex: SAR, KWD.
+         - Parameter post:                  The post url you are passing to the SDK, ot the post url you pass within the Charge API. If you are not using postUrl please pass it as empty string
+         - Parameter transactionReference:  The reference.trasnsaction you are passing to the SDK(not all SDKs supports this,) or the reference.trasnsaction  you pass within the Charge API. If you are not using reference.trasnsaction please pass it as empty string
+         */
+        static func generateTapHashString(publicKey:String, secretKey:String, amount:Double, currency:String, postUrl:String = "", transactionReference:String = "") -> String {
+            // Let us generate our encryption key
+            let key = SymmetricKey(data: Data(secretKey.utf8))
+            // For amounts, you will need to make sure they are formatted in a way to have the correct number of decimal points. For BHD we need them to have 3 decimal points
+            // We will need to format it based on the currency's decimal points
+            let numberFormatter = NumberFormatter()
+            numberFormatter.numberStyle = .currency
+            numberFormatter.currencySymbol = ""
+            numberFormatter.currencyCode = currency
+            let formattedAmount:String = numberFormatter.string(for: amount) ?? ""
+            // Let us format the string that we will hash
+            let toBeHashed = "x_publickey\(publicKey)x_amount\(formattedAmount)x_currency\(currency)x_transaction\(transactionReference)x_post\(postUrl)"
+            // let us generate the hash string now using the HMAC SHA256 algorithm
+            let signature = HMAC<SHA256>.authenticationCode(for: Data(toBeHashed.utf8), using: key)
+            let hashedString = Data(signature).map { String(format: "%02hhx", $0) }.joined()
+            return hashedString
+        }
     
     /*func setConfig(config: CardWebSDKConfig) {
         self.config = config
@@ -130,3 +165,5 @@ extension BenefitPayButtonExample: BenefitPayButtonDelegate {
         eventsTextView.text = "\n\n========\n\nonCanceled\(eventsTextView.text ?? "")"
     }
 }
+
+
